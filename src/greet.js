b782102cd001c20f727768954752099d824a3076
filfai <filippo.faiella@student.vinci.be>
2023-11-16 13:19:38 +0100

    feat: handle two names

--- src/greet.js
+++ src/greet.js
@@ -1,7 +1,12 @@
 function greet(name) {
   // null, undefined, and empty name
-  if (!name) {
+  if (!name || name.length === 0) {
     return 'Hello, my friend.';
+  }
+
+  // two names
+  if (name.length === 2) {
+    return `Hello, ${name[0]} and ${name[1]}. `;
   }
 
   // uppercase names
