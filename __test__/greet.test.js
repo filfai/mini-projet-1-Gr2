a941f63e3e76ad3ac0f55ba997ebd7b662a84190
filfai <filippo.faiella@student.vinci.be>
2023-11-16 13:16:33 +0100

    feat: handle null, undefined, and empty name

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -2,4 +2,8 @@ const greet = require('../src/greet');
 
 test('greet function', () => {
   expect(greet('Bob')).toBe('Hello, Bob.');
+
+  expect(greet(null)).toBe('Hello, my friend.');
+  expect(greet(undefined)).toBe('Hello, my friend.');
+  expect(greet('')).toBe('Hello, my friend.')
 });
