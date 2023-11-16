1ea2836877ebd7945c19ffb14737eecf464709df
filfai <filippo.faiella@student.vinci.be>
2023-11-16 15:40:41 +0100

    feat: handle multiple languages

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -1,19 +1,25 @@
 const greet = require('../src/greet');
 
 test('greet function', () => {
-  expect(greet('Bob')).toBe('Hello, Bob.');
-
+  expect(greet('Spongebob')).toBe('Hello, Spongebob.');
   expect(greet(null)).toBe('Hello, my friend.');
   expect(greet(undefined)).toBe('Hello, my friend.');
-  expect(greet('')).toBe('Hello, my friend.')
-
-  expect(greet('JERRY')).toBe('HELLO, JERRY!');
+  expect(greet('')).toBe('Hello, my friend.');
+  expect(greet('SPONGEBOB')).toBe('HELLO, SPONGEBOB!');
+  expect(greet(['Spongebob', 'Patrick'])).toBe('Hello, Spongebob and Patrick.');
+  expect(greet(['SPONGEBOB', 'Patrick'])).toBe('Hello, Patrick. AND HELLO, SPONGEBOB!');
+  expect(greet(['Spongebob', 'Patrick', 'SQUIDDY'])).toBe('Hello, Spongebob and Patrick. AND HELLO, SQUIDDY!');
+  expect(greet(['SPONGEBOB', 'Patrick', 'Squiddy'])).toBe('Hello, Patrick and Squiddy. AND HELLO, SPONGEBOB!');
   
-  expect(greet(['Jill', 'Jane'])).toBe('Hello, Jill and Jane.');
   
-  expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe('Hello, Amy, Brian and Charlotte.')
-  
-  expect(greet(['Amy', 'BRIAN'])).toBe('Hello, Amy. AND HELLO BRIAN!');
-  expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!');
+  expect(greet(['Spongebob', 'fr'])).toBe('Bonjour, Spongebob.');
+  expect(greet([null, 'fr'])).toBe('Bonjour, mon ami.');
+  expect(greet([undefined, 'fr'])).toBe('Bonjour, mon ami.');
+  expect(greet(['', 'fr'])).toBe('Bonjour, mon ami.');
+  expect(greet(['SPONGEBOB', 'fr'])).toBe('BONJOUR, SPONGEBOB!');
+  expect(greet(['Spongebob', 'Patrick', 'fr'])).toBe('Bonjour, Spongebob et Patrick.');
+  expect(greet(['SPONGEBOB', 'Patrick', 'fr'])).toBe('Bonjour, Patrick. ET BONJOUR, SPONGEBOB!');
+  expect(greet(['Spongebob', 'Patrick', 'SQUIDDY', 'fr'])).toBe('Bonjour, Spongebob et Patrick. ET BONJOUR, SQUIDDY!');
+  expect(greet(['SPONGEBOB', 'Patrick', 'Squiddy', 'fr'])).toBe('Bonjour, Patrick et Squiddy. ET BONJOUR, SPONGEBOB!');
 
 });
--- src/greet.js
+++ src/greet.js
@@ -1,25 +1,76 @@
 function greet(name) {
+
+  var language = 'en'
+  var helloString = 'Hello, '
+  var hello2String = 'HELLO, '
+  var myfriendString = 'my friend.'
+
+  var containLanguage = (Array.isArray(name) && (name[name.length-1] === 'en' || name[name.length-1] === 'fr' || name[name.length-1] === 'nl'))
+
+   if (containLanguage) {
+    if (name[name.length-1] === 'fr'){
+      language = 'fr'
+      helloString = 'Bonjour, '
+      hello2String = 'BONJOUR, '
+      myfriendString = 'mon ami.'
+    }
+    if (name[name.length-1] === 'nl'){
+      language = 'nl'
+      helloString_nl = 'Hallo, '
+      hello2String_nl = 'HALLO, '
+      myfriendString_nl = 'mijn vriend.'
+    }
+   }
+   
+   if (containLanguage){
+    var name2 = new Array(name.length-1);
+    for (let i = 0; i < name.length-1; i++) {
+      name2[i] = name[i];
+    }
+    name = name2;
+   }
+
   // null, undefined, and empty name
-  if (!name || name.length === 0) {
-    return 'Hello, my friend.';
+  if (!name || name.length === 0 || !name[0]) {
+    return helloString + myfriendString;
   }
 
   // more than one names
   if (Array.isArray(name) && name.length > 1) {
-    //const formattedNames = name.slice(0, -1).join(', ') + ' and ' + name.slice(-1);
-    //return `Hello, ${formattedNames}.`;
-    return greetmultiple(name);
+    return greetmultiple(name, language);
   }
 
   // uppercase names
-  if (name === name.toUpperCase()) {
-    return `HELLO, ${name}!`;
+  if ((typeof name === 'string' && name === name.toUpperCase()) || (Array.isArray(name) && typeof name[0] === 'string' && name[0] === name[0].toUpperCase())) {
+    return hello2String + `${name}!`;
   }
 
-  return `Hello, ${name}.`;
+  return helloString + `${name}.`;
 }
 
-function greetmultiple(names){
+function greetmultiple(names, language){
+
+  var helloString = 'Hello, '
+  var hello2String = 'HELLO, '
+  var andString = ' and '
+  var and2String = ' AND '
+  var myfriendString = 'my friend.'
+
+    if (language === 'fr'){
+      helloString = 'Bonjour, '
+      hello2String = 'BONJOUR, '
+      andString = ' et '
+      and2String = ' ET '
+      myfriendString = 'mon ami.'
+    }
+    if (language === 'nl'){
+      helloString_nl = 'Hallo, '
+      hello2String_nl = 'HALLO, '
+      andString_nl = ' en '
+      and2String_nl = ' EN '
+      myfriendString_nl = 'mijn vriend.'
+    }
+
   const normalNames = [];
   const uppercaseNames = [];
 
@@ -34,19 +85,19 @@ function greetmultiple(names){
   let result = '';
 
   if (normalNames.length === 1) {
-    result += `Hello, ${normalNames[0]}.`;
+    result += helloString + normalNames[0] + '.';
   }
   if (normalNames.length > 1) {
-    const formattedNames = normalNames.slice(0, -1).join(', ') + ' and ' + normalNames.slice(-1);
-    result += `Hello, ${formattedNames}.`;
+    const formattedNames = normalNames.slice(0, -1).join(', ') + andString + normalNames.slice(-1);
+    result += helloString + formattedNames + '.';
   }
 
   if (uppercaseNames.length === 1) {
-    result += ` AND HELLO ${uppercaseNames[0]}!`;
+    result += and2String +  hello2String + uppercaseNames[0] + '!';
   }
   if (uppercaseNames.length > 1) {
-    const formattedNames = uppercaseNames.slice(0, -1).join(', ') + ' AND ' + uppercaseNames.slice(-1);
-    result += ` AND HELLO ${formattedNames}!`;
+    const formattedNames = uppercaseNames.slice(0, -1).join(', ') + and2String + uppercaseNames.slice(-1);
+    result += and2String +  hello2String + formattedNames + '!';
   }
 
   return result.trim();
